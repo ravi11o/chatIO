6034b51cffb1f9825e73eef5e137322103ef4567
suraj kumar <ravi.suraj110@gmail.com>
2016-10-12 10:10:30 +0530

    Create Username and Activate chat over Socket

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server);
-
+var usernames = [];
 
 server.listen(process.env.PORT || 3000); 
 
@@ -11,10 +11,34 @@ app.get('/', function(req, res) {
 });
 
 io.sockets.on('connection', function(socket) {
+
+    socket.on('new user', function(data, callback) {
+        if(usernames.indexOf(data) != -1) {
+            callback(false);
+        } else {
+            callback(true);
+            socket.username = data;
+            usernames.push(socket.username);
+            updateUsernames();
+        }
+    });
+    // update Usernames
+    function updateUsernames() {
+        io.sockets.emit('usernames', usernames);
+    }
+
     //Send Message
     socket.on('send message', function(data) {
         io.sockets.emit('new message', {
-            msg: data
+            msg: data,
+            user: socket.username
         });
+    });
+
+    // Delete Username
+    socket.on('disconnect', function(data) {
+        if(!socket.username) return;
+        usernames.splice(usernames.indexOf(socket.username), 1);
+        updateUsernames();
     });
 });
